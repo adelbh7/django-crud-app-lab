385e76986cba2f02f77b3e0ad49fdcc5de1fe776
adelbh7 <adelbh7@gmail.com>
2025-05-19 20:30:39 +0300

    lab

--- myproject/my_app/models.py
+++ myproject/my_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'my_app',
+    'django.contrib.auth',
+    'labapp',
+
 ]
 
 MIDDLEWARE = [
